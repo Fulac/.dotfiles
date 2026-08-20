7baa3db67041da3a0d669dc605fd04d6c854a072
fulac <full3908@gmail.com>
2026-08-21 03:28:10 +0900

    lua用のlinter(selene)を導入し、seleneの設定ファイルを新規追加

--- config/nvim/lua/plugins/linter.lua
+++ config/nvim/lua/plugins/linter.lua
@@ -11,6 +11,12 @@ return {
       lua = { "selene" },
     }
 
+    -- selene.toml の置き場所を固定するため、cwdをdotfiles側に明示的に指定
+    lint.linters.selene.args = vim.list_extend(
+      { "--config", vim.fn.stdpath("config") .. "/selene.toml" },
+      lint.linters.selene.args or {}
+    )
+
     -- 編集や保存のタイミングで自動的にリンターを走らせるオートコマンド
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
